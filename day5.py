83c90caed3efe84f687728a66013c20ec911aa2a
Manas Ranjan Bera <manasbera484@gmail.com>
2026-02-12 11:50:15 +0530

    notes

---
+++ day5.py
@@ -0,0 +1,48 @@
+# count = 0
+# for i in range(1,5):
+#     for j in range(1,5):
+#         print(i,j)
+#         count += 1
+# print(count)
+
+# count=0
+# for i in range(5):
+#     for j in range(5):
+#         for k in range(5):
+#             print(i,j,k)
+#             count+=1
+# print(count)
+
+# for i in range(1,50):
+#     if(i%2==0):
+#         # break
+#         continue
+#     print(i)
+
+# String and It's Inbuilt methods
+s1 = "       This is a a a a string      "
+s2 = "This is Another string"
+# print(s1[2])
+# print(s1[-2])
+# print(s1[1:6])
+# print(s2[3:10])
+# print(s2[6:])
+# print(s2[:6])
+# print(s2[:])
+# print(s1+s2)
+# print(len(s1))
+
+# print(s2.upper())
+# print(s2.lower())
+# print(s2.capitalize())
+# print(s2.title())
+# print(s2.split())
+# print(s2.split('is'))
+# s3 = s2.split()
+# print(s3)
+# print('hello'.join(s3))
+# print(s2.replace('is','was'))
+# print(s1)
+# print(s1.strip())
+# print(s1.lstrip())
+# print(s1.rstrip())
